d526d59c74e809ab1d0b589c7058837d0e0d8ee6
ekyeon715 <rkdwldus9636@naver.com>
2024-08-02 09:28:45 +0900

    상세페이지 입력

--- js/detailPage.js
+++ js/detailPage.js
@@ -1,1 +1,38 @@
-import { movies } from "./fetchData.js";
+import { movies, fetchMovies } from "./fetchData.js";
+// console.log(movies);
+fetchMovies().then(() => {
+    createDetailPage(movies);
+})
+
+
+
+// 상세페이지 데이터 입력
+function createDetailPage() {
+    const movieInfo = document.createElement('div');
+    const movie = movies
+    console.log(movie);
+    movieInfo.className = 'topPosition';
+    movieInfo.innerHTML = `
+    <div class="moviePic">
+                <img src=""https://image.tmdb.org/t/p/w500${movie.poster_path}"" alt="${movie.title}">
+            </div>
+
+
+            <div class="contents">
+                <div class="contents1">
+                    <div class="theme"><h3>${movie.title}</h3> </div>
+                    <div class="summary"><p>${movie.overview}</p></div>
+                </div>
+
+                <div class="contents2">
+                    <div class="genre">장르</div>
+                    <div class="date">${movie.relase_date}</div>
+                    <div class="rate"><span>평점: ${movie.vote_average}</span></div>
+                </div>
+            </div>
+`;
+    // card.addEventListener('click', () => alert(`Movie ID: ${movie.id}`));
+    return movieInfo;
+    
+}
+
